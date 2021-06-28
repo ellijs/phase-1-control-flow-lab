3b1508ee6b9c345617b0a38427c523fcfeaba0a3
ellijs <ellijs@hotmail.com>
2021-06-28 12:54:56 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (2000 < distance && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result;
+  city === "NYC" ? (result = "Ok, sounds good.") : (result = "No go.");
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+
+    case "not as generous":
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+  }
 }
+scuberGreetingForFeet(199);
+ternaryCheckCity("NYC");
+switchOnCharmFromTip();
